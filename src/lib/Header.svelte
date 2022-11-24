b7a12eefe7391058d87724608acb893154de451d
hatsumi3 <al.usamiku39@gmail.com>
2022-11-24 17:55:26 +0900

    refactor:adjust UI(header, nerdle space) and prepare related service worker.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import svelteLogo from "./../assets/svelte.svg";
+</script>
+
+<header class="header">
+  <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  <div class="logo nerdle">
+    <span class="nerdle-name" style="font-size: 1.8rem;">nerdle</span>
+    <span class="nerdle-dot">.</span>
+  </div>
+</header>
+
+<style>
+  .header {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+  .logo.nerdle {
+    margin-left: auto;
+  }
+</style>
