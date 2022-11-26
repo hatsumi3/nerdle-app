56f9a962abc84891072ca144df2f0903739c5ff3
hatsumi3 <al.usamiku39@gmail.com>
2022-11-26 19:01:13 +0900

    feat: add game check.
    refactor: summalize gridSummary.

--- src/lib/NerdleGame.svelte
+++ src/lib/NerdleGame.svelte
@@ -113,19 +113,22 @@
       case gameStates.success:
         nerdleAlertUtil.gameClear();
         fixed();
-        return;
+        break;
       case gameStates.continue:
         moveNextRow();
-        return;
+        break;
       case formulaStates.error:
         nerdleAlertUtil.formulaError();
-        return;
+        break;
       case gameStates.failed:
       default:
         nerdleAlertUtil.gameFailed(nerdle.problem);
         fixed();
-        return;
+        break;
     }
+    //リアクティビティが代入によってトリガー
+    nerdle = nerdle;
+    console.log(nerdle.grid);
   }
 </script>
 
@@ -174,72 +177,7 @@
           nerdleSetValue(sign);
           moveRight();
         }}
-      >
-        {#if sign === "+"}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            aria-label="plus"
-            style="height: 1rem"
-          >
-            <title id="plustitle">plus</title>
-            <path
-              d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"
-            />
-          </svg>
-        {/if}
-        {#if sign === "-"}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            aria-label="minus"
-            style="height: 1rem"
-          >
-            <title id="minustitle">minus</title>
-            <path
-              d="M400 288h-352c-17.69 0-32-14.32-32-32.01s14.31-31.99 32-31.99h352c17.69 0 32 14.3 32 31.99S417.7 288 400 288z"
-            />
-          </svg>
-        {/if}
-        {#if sign === "*"}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            aria-label="multipy"
-            style="height: 1rem"
-          >
-            <title id="timestitle">multiply</title>
-            <path
-              d="M417.1 368c-5.937 10.27-16.69 16-27.75 16c-5.422 0-10.92-1.375-15.97-4.281L256 311.4V448c0 17.67-14.33 32-31.1 32S192 465.7 192 448V311.4l-118.3 68.29C68.67 382.6 63.17 384 57.75 384c-11.06 0-21.81-5.734-27.75-16c-8.828-15.31-3.594-34.88 11.72-43.72L159.1 256L41.72 187.7C26.41 178.9 21.17 159.3 29.1 144C36.63 132.5 49.26 126.7 61.65 128.2C65.78 128.7 69.88 130.1 73.72 132.3L192 200.6V64c0-17.67 14.33-32 32-32S256 46.33 256 64v136.6l118.3-68.29c3.838-2.213 7.939-3.539 12.07-4.051C398.7 126.7 411.4 132.5 417.1 144c8.828 15.31 3.594 34.88-11.72 43.72L288 256l118.3 68.28C421.6 333.1 426.8 352.7 417.1 368z"
-            />
-          </svg>
-        {/if}
-        {#if sign === "/"}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 512"
-            aria-label="divide"
-            style="height: 1rem; transform: rotate(90deg)"
-          >
-            <title id="divtitle">divide</title>
-            <path
-              d="M5.112 9.196C13.29-1.236 28.37-3.065 38.81 5.112L630.8 469.1C641.2 477.3 643.1 492.4 634.9 502.8C626.7 513.2 611.6 515.1 601.2 506.9L9.196 42.89C-1.236 34.71-3.065 19.63 5.112 9.196V9.196z"
-            />
-          </svg>
-        {/if}
-        {#if sign === "="}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            aria-label="equals"
-            style="height: 1rem"
-          >
-            <title id="equalstitle">=</title>
-            <path
-              d="M48 192h352c17.69 0 32-14.32 32-32s-14.31-31.1-32-31.1h-352c-17.69 0-32 14.31-32 31.1S30.31 192 48 192zM400 320h-352c-17.69 0-32 14.31-32 31.1s14.31 32 32 32h352c17.69 0 32-14.32 32-32S417.7 320 400 320z"
-            />
-          </svg>
-        {/if}
+        >{sign}
       </button>
     {/each}
     <button
@@ -260,9 +198,6 @@
     </button>
   </div>
 </div>
-
-<div>{selectedCell.row} - {selectedCell.column}</div>
-<div>{typeof selectedCell.row} - {typeof selectedCell.column}</div>
 
 <style>
   /* グリッド */
@@ -295,14 +230,17 @@
     background-color: #989484;
   }
   .block.not-included {
+    color: #fff;
     border-color: rgb(22 24 3);
     background-color: rgb(22 24 3);
   }
   .block.included {
+    color: #fff;
     border-color: rgb(130 4 88);
     background-color: rgb(130 4 88);
   }
   .block.correct {
+    color: #fff;
     border-color: rgb(57 136 116);
     background-color: rgb(57 136 116);
   }
@@ -343,14 +281,17 @@
     background-color: rgb(148 163 184);
   }
   .input-button.not-included {
+    color: #fff;
     border-color: rgb(22 24 3);
     background-color: rgb(22 24 3);
   }
   .input-button.included {
+    color: #fff;
     border-color: rgb(130 4 88);
     background-color: rgb(130 4 88);
   }
   .input-button.correct {
+    color: #fff;
     border-color: rgb(57 136 116);
     background-color: rgb(57 136 116);
   }
--- src/lib/nerdle.js
+++ src/lib/nerdle.js
@@ -2,10 +2,19 @@
 import { cellStates, formulaStates, gameStates } from "./const";
 
 const createProblem = (column, mode = {}) => {
-    // TODO: column, modeによって変更できるようにする
+    // TODO: column, modeによって,問題の候補を変更できるようにする
     const candidate =
-        ["12+35=47", "89-76=13", "1*2*3=6", "9/3+2=5", "22+22=44"]
+        ["12+32=44", "12+35=47", "89-76=13", "1*3*8=24", "9/3+8=11"]
     return candidate[Math.floor(Math.random() * candidate.length)]
+}
+
+const createCountStrObject = (str) => {
+    let retObj = {}
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in retObj) retObj[str[i]] += 1;
+        else retObj[str[i]] = 1;
+    }
+    return retObj
 }
 
 /**
@@ -14,7 +23,13 @@ const createProblem = (column, mode = {}) => {
  * nerdle.gird: row * columnの2次元配列.cellにはvalue,stateを持つオブジェクト.
  * nerdle.grid.value: 数値、記号.
  * nerdle.grid.state: cellStatesのいずれか.
- * nerdle.setvalue: gridへのsetter. cell.row, cell.columnへvalueを挿入.
+ * nerdle.setValue: gridへのsetter. cell.row, cell.columnへvalueを挿入.
+ * nerdle.setState: gridへのsetter. cell.row, cell.columnへstateを挿入.
+ * nerdle.problem: 問題文字列.
+ * nerdle.gameState: ゲーム状態.
+ * nerdle.verify: 数式、ゲーム状態の検証.
+ * nerdle.verifyGameState: 数式の検証.
+ * nerdle.verifyformulaState: ゲーム状態の検証.
  * 
  * @param {number} row
  * @param {number} column
@@ -22,6 +37,9 @@ const createProblem = (column, mode = {}) => {
  * @return {object} nerdle object
  */
 const createNerdle = (row, column, mode = {}) => {
+
+    const problem = createProblem(column, mode);
+    const problemCountStr = createCountStrObject(problem);
 
     const nerdle = {
         rowSize: row,
@@ -36,7 +54,18 @@ const createNerdle = (row, column, mode = {}) => {
             if (!(cell.column >= 0 && cell.column < this.columnSize)) return;
             this.grid[cell.row][cell.column] = { ...this.grid[cell.row][cell.column], value };
         },
-        problem: createProblem(column, mode),
+        setState: function (cell, state) {
+            if (!(cell.row >= 0 && cell.row < this.rowSize)) return;
+            if (!(cell.column >= 0 && cell.column < this.columnSize)) return;
+            this.grid[cell.row][cell.column] = { ...this.grid[cell.row][cell.column], state };
+        },
+        updateSummaryState: function (key, state) {
+            const currentState = this.gridSummary[key]
+            if (currentState === cellStates.correct || currentState === cellStates.notIncluded) return;
+            this.gridSummary[key] = state;
+        },
+        problem,
+        problemCountStr,
         gameState: gameStates.continue,
         verify: function (row) {
             let formula = ""
@@ -45,14 +74,68 @@ const createNerdle = (row, column, mode = {}) => {
             }
             const formulaState = this.verifyFormulaState(formula)
             if (formulaState === formulaStates.error) return formulaState
-            const gameState = this.verifyGameState(formula)
+            const gameState = this.verifyGameState(row, formula)
             return gameState
         },
-        verifyGameState: function (formula) {
+        verifyGameState: function (row, formula) {
+            // 1. 1文字づつ比較,grid,gridSummary更新.
+            for (let j = 0; j < this.columnSize; j++) {
+                let index = this.problem.indexOf(formula[j]);
+                // 含まれない場合.
+                if (index === -1) {
+                    this.setState({ row, column: j }, cellStates.notIncluded);
+                    this.updateSummaryState(formula[j], cellStates.notIncluded)
+                    continue;
+                }
+                // 正しい場合.
+                if (this.problem[j] === formula[j]) {
+                    this.setState({ row, column: j }, cellStates.correct)
+                    this.updateSummaryState(formula[j], cellStates.correct)
+                    continue;
+                }
+                // 含まれる場合.
+                this.setState({ row, column: j }, cellStates.included)
+                this.updateSummaryState(formula[j], cellStates.included)
+            }
 
-            // return gameStates.failed
-            return gameStates.continue
-            // return gameStates.success;
+            // 2. 複数文字対応(文字数に応じてgridのstateを変更), grid更新
+            const rowCountStr = createCountStrObject(formula)
+            for (const [key, count] of Object.entries(rowCountStr)) {
+                // 2文字以上含まれる場合
+                // 問題に文字が含まれるかを確認
+                if (count <= 1) continue;
+                if (!(key in this.problemCountStr)) continue;
+                // 文字数が問題の文字数以下か確認
+                let problemKeyCount = this.problemCountStr[key]
+                if (count <= problemKeyCount) continue;
+                // 文字数が合うまで,stateの変更を行う
+                let changeCount = count - problemKeyCount;
+                console.log(key, changeCount)
+                for (let j = 0; j < this.columnSize; j++) {
+                    if (this.grid[row][j].value != key) continue;
+                    if (this.grid[row][j].state === cellStates.correct) continue;
+                    this.setState({ row, column: j }, cellStates.notIncluded)
+                    changeCount -= 1;
+                    if (changeCount <= 0) break;
+                }
+            }
+
+            // 3. ゲーム状態判定
+            let isClear = true
+            for (let j = 0; j < this.columnSize; j++) {
+                if (this.grid[row][j].state !== cellStates.correct) {
+                    isClear = false;
+                    break;
+                }
+            }
+            if (isClear) {
+                this.gameState = gameStates.success;
+            } else if (row === this.rowSize - 1) {
+                this.gameState = gameStates.failed;
+            } else {
+                this.gameState = gameStates.continue;
+            }
+            return this.gameState
         },
         verifyFormulaState: function (formula) {
             // 1. =が1つのみ
@@ -63,7 +146,7 @@ const createNerdle = (row, column, mode = {}) => {
             const regexNum = /^[1-9][0-9]*$/
             if (!regexNum.test(formulaArray[1])) return formulaStates.error;
 
-            // 3. 左辺に記号が連続しない、記号の間は数値である
+            // 3. 左辺に記号が連続しない、かつ記号の間は数値である
             const regexSign = /\+|-|\*|\//
             const leftFormula = formulaArray[0].split(regexSign)
             if (leftFormula.includes("")) return formulaStates.error
